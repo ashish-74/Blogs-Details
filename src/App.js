bcb8094a5a51b5bf88b791ba28189d1f81d1d7d2
unknown <ashish.kumar7497@gmail.com>
2021-04-18 03:22:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/create" component={Create} />
+            <Route path="/blogs/:id" component={BlogDetails} />
+            <Route path="*" component={NotFound}/>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
